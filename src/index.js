0c39c6ebac9ad5a405cb41101f60a854e1e694be
KITTIPHAT SRIYAM <31922824+KittiphatSriyam@users.noreply.github.com>
2021-04-10 08:52:03 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MmPage from "./page/mm-page";
 
 import "./scss/app.scss";
 
-const App = () => {
-  return <MmPage />;
-};
+const App = () => <p>Hello</p>
 ReactDOM.render(<App />, document.getElementById("app"));
